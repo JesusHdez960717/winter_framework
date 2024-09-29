2f3df78b83b453e8ea2ede92580f4e88efe47332
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-09-29 18:02:54 -0400

    Add filter chain example

---
+++ bin/example/filter_chain/server.dart
@@ -0,0 +1,47 @@
+import '../../winter/winter.dart';
+
+void main() => WinterServer(
+      config: ServerConfig(port: 9090),
+      filterConfig: FilterConfig(
+        [
+          RemoveQueryParamsFilter(),
+          //LogsFilter(),
+          RateLimiterFilter(
+            onRequest: (request) =>
+                request.headers['X-Request-ID'] ?? 'unknown',
+            rateLimiter: RateLimiter(
+              1,
+              const Duration(minutes: 1),
+            ),
+            log: (request, requestId) {
+              print('Rate limiter fail');
+            },
+          )
+        ],
+      ),
+      router: SimpleWinterRouter(
+        config: RouterConfig(
+          onInvalidUrl: OnInvalidUrl.fail(),
+        ),
+        routes: [
+          WinterRoute(
+            path: '/filter-chain',
+            method: HttpMethod.get,
+            handler: (request) => ResponseEntity.ok(
+              body: request.queryParams,
+            ),
+          ),
+        ],
+      ),
+    ).start();
+
+class RemoveQueryParamsFilter implements Filter {
+  @override
+  Future<ResponseEntity> doFilter(
+    RequestEntity request,
+    FilterChain chain,
+  ) async {
+    request.queryParams.clear();
+    return await chain.doFilter(request);
+  }
+}
