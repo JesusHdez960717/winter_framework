3f2d8af109c92e1b1694204697ac7dffaee42fc4
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-10-11 16:29:02 -0400

    BasicRoute: add filter config as a param in every add

--- bin/winter/router/basic_router.dart
+++ bin/winter/router/basic_router.dart
@@ -4,36 +4,136 @@ class BasicRouter extends WinterRouter {
   BasicRouter({
     super.basePath,
     List<Route>? routes,
-    RouterConfig? config,
   }) : super(
           routes: routes ?? [],
-          config: config ?? RouterConfig(),
         );
 
-  void add(String path, HttpMethod method, WinterHandler handler) =>
-      routes.add(Route(path: path, method: method, handler: handler));
+  void addRoute(Route route) => routes.add(route);
 
-  void get(String path, WinterHandler handler) =>
-      routes.add(Route(path: path, method: HttpMethod.get, handler: handler));
+  void add(
+    String path,
+    HttpMethod method,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: method,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void query(String path, WinterHandler handler) =>
-      routes.add(Route(path: path, method: HttpMethod.query, handler: handler));
+  void get(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.get,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void post(String path, WinterHandler handler) =>
-      routes.add(Route(path: path, method: HttpMethod.post, handler: handler));
+  void query(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.query,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void put(String path, WinterHandler handler) =>
-      routes.add(Route(path: path, method: HttpMethod.put, handler: handler));
+  void post(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.post,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void patch(String path, WinterHandler handler) =>
-      routes.add(Route(path: path, method: HttpMethod.patch, handler: handler));
+  void put(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.put,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void delete(String path, WinterHandler handler) => routes
-      .add(Route(path: path, method: HttpMethod.delete, handler: handler));
+  void patch(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.patch,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void head(String path, WinterHandler handler) =>
-      routes.add(Route(path: path, method: HttpMethod.head, handler: handler));
+  void delete(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.delete,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 
-  void options(String path, WinterHandler handler) => routes
-      .add(Route(path: path, method: HttpMethod.options, handler: handler));
+  void head(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.head,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
+
+  void options(
+    String path,
+    WinterHandler handler, {
+    FilterConfig? filterConfig,
+  }) =>
+      routes.add(
+        Route(
+          path: path,
+          method: HttpMethod.options,
+          handler: handler,
+          filterConfig: filterConfig,
+        ),
+      );
 }
