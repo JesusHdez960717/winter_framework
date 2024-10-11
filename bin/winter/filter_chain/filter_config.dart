798e97ebba15e439f7007b97404ffa26513ef865
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-10-11 16:25:04 -0400

    FilterConfig: add merge & toString

--- bin/winter/filter_chain/filter_config.dart
+++ bin/winter/filter_chain/filter_config.dart
@@ -3,5 +3,14 @@ import 'filter.dart';
 class FilterConfig {
   final List<Filter> filters;
 
-  FilterConfig(this.filters);
+  const FilterConfig(this.filters);
+
+  FilterConfig merge(FilterConfig? other) {
+    return FilterConfig([...filters, ...(other?.filters ?? [])]);
+  }
+
+  @override
+  String toString() {
+    return '$filters';
+  }
 }
