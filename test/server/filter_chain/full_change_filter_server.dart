d5a7a6abe81ac3890ba796903b58c1f6fd70f459
jessica.garcia <jgarciaalbalah@gmail.com>
2024-10-19 23:57:05 -0400

    Add tests

---
+++ test/server/filter_chain/full_change_filter_server.dart
@@ -0,0 +1,80 @@
+@TestOn('vm')
+library;
+
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
+
+import '../../../bin/winter/winter.dart';
+
+void main() {
+  int port = 9090;
+  String localUrl = 'http://localhost:$port';
+
+  setUp(
+    () async {
+      await Winter.run(
+        config: ServerConfig(port: port),
+        globalFilterConfig: FilterConfig([FullChangeFilterFilter()]),
+        router: WinterRouter(
+          routes: [
+            Route(
+              path: '/full-change-filter',
+              method: HttpMethod.post,
+              handler: (request) async {
+                return ResponseEntity.ok(
+                  body: await request.body<String>(),
+                  headers: {
+                    'before-request-header':
+                        request.headers['before-request-header'] ?? '',
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      );
+    },
+  );
+
+  tearDown(() => Winter.close(force: true));
+
+  Uri url(String path) => Uri.parse(localUrl + path);
+
+  test('Test Full Change Filter', () async {
+    String urlToTest = '/full-change-filter';
+
+    String body = 'Hello world!!!';
+    http.Response response = await http.post(url(urlToTest), body: body);
+
+    expect(response.statusCode, 200);
+    expect(response.body, body);
+    expect(response.headers['before-request-header'], 'before-request');
+    expect(response.headers['after-request-header'], 'after-request');
+  });
+}
+
+class FullChangeFilterFilter implements Filter {
+  @override
+  Future<ResponseEntity> doFilter(
+    RequestEntity request,
+    FilterChain chain,
+  ) async {
+    RequestEntity newRequestEntity = request.copyWith(
+      headers: {
+        ...request.headers,
+        'before-request-header': 'before-request',
+      },
+    );
+
+    ResponseEntity newResponseEntity = await chain.doFilter(newRequestEntity);
+
+    newResponseEntity = newResponseEntity.copyWith(
+      headers: {
+        ...newResponseEntity.headers,
+        'after-request-header': 'after-request',
+      },
+    );
+
+    return newResponseEntity;
+  }
+}
--- test/server/filter_chain/mixed_filter_server.dart
+++ test/server/filter_chain/mixed_filter_server.dart
@@ -14,10 +14,11 @@ void main() {
     () async {
       await Winter.run(
         config: ServerConfig(port: port),
+        globalFilterConfig: FilterConfig([InterceptNotAuthRequestsFilter()]),
         router: WinterRouter(
           routes: [
             Route(
-              path: '/route-filter/{id}',
+              path: '/mixed-filter/{id}',
               filterConfig: FilterConfig([RemoveQueryParamsFilter()]),
               method: HttpMethod.get,
               handler: (request) => ResponseEntity.ok(
@@ -26,7 +27,7 @@ void main() {
               ),
             ),
             Route(
-              path: '/route-filter/2/{id}',
+              path: '/mixed-filter/2/{id}',
               method: HttpMethod.get,
               handler: (request) => ResponseEntity.ok(
                 body:
@@ -43,18 +44,40 @@ void main() {
 
   Uri url(String path) => Uri.parse(localUrl + path);
 
-  test('Test Route Filter', () async {
-    String urlToTest = '/route-filter/55?some=123&another=963';
+  ///No auth header provided, give a 401 (test on route #1)
+  test('Test No Auth Filter', () async {
+    String urlToTest = '/mixed-filter/55';
     http.Response response = await http.get(url(urlToTest));
+    expect(response.statusCode, 401);
+  });
+
+  ///No auth header provided, give a 401 (test on route #1)
+  test('Test No Auth Filter', () async {
+    String urlToTest = '/mixed-filter/2/55';
+    http.Response response = await http.get(url(urlToTest));
+    expect(response.statusCode, 401);
+  });
+
+  ///Auth header provided, give 200, second filter remove params, give body without params
+  test('Test Route filter after pass auth filter', () async {
+    String urlToTest = '/mixed-filter/55';
+    http.Response response = await http.get(
+      url(urlToTest),
+      headers: {HttpHeaders.AUTHORIZATION: 'Bearer 123456'},
+    );
     expect(response.statusCode, 200);
 
     ///body with empty params
     expect(response.body, 'path: {}, query: {}');
   });
 
-  test('Test Route Filter #2', () async {
-    String urlToTest = '/route-filter/2/55?some=123&another=963';
-    http.Response response = await http.get(url(urlToTest));
+  ///Auth header provided, give 200, no second filter, return body with params
+  test('Test Route filter after pass auth filter', () async {
+    String urlToTest = '/mixed-filter/2/55?some=123&another=963';
+    http.Response response = await http.get(
+      url(urlToTest),
+      headers: {HttpHeaders.AUTHORIZATION: 'Bearer 123456'},
+    );
     expect(response.statusCode, 200);
 
     ///body with current params (not removed by filter)
@@ -62,7 +85,22 @@ void main() {
   });
 }
 
-///This filter remove the path & query params of the request
+class InterceptNotAuthRequestsFilter implements Filter {
+  @override
+  Future<ResponseEntity> doFilter(
+    RequestEntity request,
+    FilterChain chain,
+  ) async {
+    if (!request.headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+      return ResponseEntity(
+        401,
+        body: 'Request need the ${HttpHeaders.AUTHORIZATION} header',
+      );
+    }
+    return await chain.doFilter(request);
+  }
+}
+
 class RemoveQueryParamsFilter implements Filter {
   @override
   Future<ResponseEntity> doFilter(
