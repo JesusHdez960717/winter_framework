b18f5ba4405871dee744646bb5fb07b70f557189
jessica.garcia <jgarciaalbalah@gmail.com>
2024-10-20 12:11:35 -0400

    Add exception handler test

---
+++ test/exception_handler/raw_exception_handler.dart
@@ -0,0 +1,82 @@
+@TestOn('vm')
+library;
+
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
+
+import '../../bin/winter/winter.dart';
+
+void main() {
+  int port = 9090;
+  String localUrl = 'http://localhost:$port';
+
+  ExceptionHandler exc = TestExceptionHandler();
+
+  setUp(
+    () async {
+      await Winter.run(
+        config: ServerConfig(port: port),
+        context: BuildContext(exceptionHandler: exc),
+        router: WinterRouter(
+          routes: [
+            Route(
+              path: '/exception/1',
+              method: HttpMethod.get,
+              handler: (request) =>
+                  throw TestException(message: 'Error from /exception'),
+            ),
+            Route(
+              path: '/exception/2',
+              method: HttpMethod.get,
+              handler: (request) => ResponseEntity.ok(
+                body: 'Hello world!!!',
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+
+  tearDown(() => Winter.close(force: true));
+
+  Uri url(String path) => Uri.parse(localUrl + path);
+
+  test('Test Exception #1', () async {
+    String urlToTest = '/exception/1';
+    http.Response response = await http.get(url(urlToTest));
+    expect(response.statusCode, 400);
+    expect(response.body, 'Error from /exception');
+  });
+
+  test('Test Exception #2', () async {
+    String urlToTest = '/exception/2';
+    http.Response response = await http.get(url(urlToTest));
+    expect(response.statusCode, 200);
+    expect(response.body, 'Hello world!!!');
+  });
+}
+
+class TestException implements Exception {
+  final String message;
+
+  TestException({
+    required this.message,
+  });
+
+  @override
+  String toString() {
+    return message;
+  }
+}
+
+class TestExceptionHandler extends ExceptionHandler {
+  @override
+  Future<ResponseEntity> call(
+    RequestEntity request,
+    Exception exception,
+    StackTrace stackTrac,
+  ) async {
+    return ResponseEntity.badRequest(body: exception.toString());
+  }
+}
