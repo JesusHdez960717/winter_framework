1b3add036f15c8337c84782803b4846ed174c271
Jesus Hernandez Barrios <jhernandezb96@gmail.com>
2024-10-26 12:35:05 -0400

    Fix tests for route config changes
    Add test for ignore & fail on wrong path

--- test/server/winter_router/h_route/simple_h_router_test.dart
+++ test/server/winter_router/h_route/simple_h_router_test.dart
@@ -6,7 +6,7 @@ import 'package:test/test.dart';
 import 'package:winter/winter.dart';
 
 void main() {
-  int port = 9052;
+  int port = 9053;
   String localUrl = 'http://localhost:$port';
 
   setUpAll(
